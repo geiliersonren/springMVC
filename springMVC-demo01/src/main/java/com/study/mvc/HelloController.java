fce4cfc54fb48a54588615b2f62235cc986cc0c5
superStar <lin18846443196@outlook.com>
2023-12-21 22:49:24 +0800

    hello springmvc

---
+++ springMVC-demo01/src/main/java/com/study/mvc/HelloController.java
@@ -0,0 +1,29 @@
+package com.study.mvc;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * ClassName: HelloController
+ * Package: com.study.mvc
+ * Description:
+ *
+ * @Author 林七七
+ * @Create 2023/12/21 22:24
+ * @Version 1.0
+ */
+
+@Controller
+public class HelloController {
+
+    @RequestMapping(value = "/")
+    public String getindex() {
+        return "index";
+    }
+
+    @RequestMapping(value = "/hello")
+    public String getHello() {
+
+        return "hello";
+    }
+}
